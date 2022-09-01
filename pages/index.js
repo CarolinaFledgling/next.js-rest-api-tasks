1b2715110ae9834282333254cbb81d0fb3c2fa83
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-09-01 14:41:03 +0200

    added description for first task

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,19 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome 
-        </h1>
+        <div className={styles.description}>
+          <p >
+            Task nr 1 
+          </p>
+          <p>Create input  where you will write the random name of the country and  return you will show the weather for that country's capital
+            e.g, I will write : Poland
+            I should get Warsaw and the weather  information for the capital.</p>
+        </div>
 
 
-   
+
+
+
       </main>
 
 
