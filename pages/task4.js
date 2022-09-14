34c15b0b458cdb02fd773401a2dcd1ec371d5a7c
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-09-14 19:50:37 +0200

    fixing getWEather function with displaying loading

--- pages/task4.js
+++ pages/task4.js
@@ -57,6 +57,7 @@ export default function Home() {
             })
             .finally(() => {
                 setIsSaveLoading(false);
+                setIsLoading(false)
             });
     };
 
